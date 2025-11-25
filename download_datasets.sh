08010b5718701b7a410fe3d0f98498dadcdb31cc
Anna <anna@porinzip.com>
2025-11-25 18:22:53 +0100

    - update download_datasets to include the accessible STEL tasks - fix order alignment to expect at least 2 in tgt not src embedding

--- download_datasets.sh
+++ download_datasets.sh
@@ -8,6 +8,7 @@ rm -rf hate-speech-and-offensive-language
 rm -rf CharCnn_Keras
 rm -rf enron_authorship_corpus
 rm -rf 'Enron (80 authors)'
+rm -rf STEL
 rm -f blogs.zip
 rm -f n77w7mygwg-1.zip
 rm -f Enron-Authorship-Verification-Corpus.zip
@@ -17,7 +18,7 @@ rm -rf hate-speech-dataset/.git
 git clone --depth 1 https://github.com/t-davidson/hate-speech-and-offensive-language.git
 rm -rf hate-speech-and-offensive-language/.git
 git clone --depth 1 https://github.com/mhjabreel/CharCnn_Keras.git
-rm -rf CharCnn_Keras/.git
+rm -rf CharCnn_Keraalso as/.git
 
 # Note: The blog_authorship_corpus download is currently disabled due to an unreliable server.
 # curl -k -O http://www.cs.biu.ac.il/~koppel/blogs/blogs.zip
@@ -32,3 +33,9 @@ mv 'Enron (80 authors)' enron_authorship_corpus
 gdown https://drive.google.com/file/d/17eLjiDLhXXtrfqC1FzToHKrwTcwsAolN/view?usp=sharing --fuzzy
 tar zxvf jigsaw-toxic-comment-classification-challenge.tar.gz
 rm jigsaw-toxic-comment-classification-challenge.tar.gz
+
+git clone --depth 1 --filter=blob:none --sparse https://github.com/nlpsoc/STEL.git STEL
+cd STEL
+git sparse-checkout set Data/STEL
+rm -rf .git
+cd ..
